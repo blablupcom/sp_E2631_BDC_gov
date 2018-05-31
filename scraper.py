18e6435bf302edc935bb642c21ee87969177f5b7
blablupcom <o.volodin@yahoo.com>
2018-05-31 19:59:52 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -85,8 +85,8 @@ def convert_mth_strings ( mth_string ):
 
 #### VARIABLES 1.0
 
-entity_id = "E0931_ABC_gov"
-url = "https://www.allerdale.gov.uk/en/about-council/budget-and-spending/spending-council/"
+entity_id = "E2631_BDC_gov"
+url = "https://www.breckland.gov.uk/article/4248/Council-Expenditure"
 errors = 0
 data = []
 
@@ -94,19 +94,32 @@ data = []
 #### READ HTML 1.0
 
 html = urllib2.urlopen(url)
-soup = BeautifulSoup(html, "lxml")
+soup = BeautifulSoup(html, 'lxml')
 
 #### SCRAPE DATA
 
-links = soup.find_all('a')
+links = soup.find('div', 'grid grid--1col grid--list').find_all('a', href=True)
 for link in links:
-    file_name = link.text
-    if 'Spending' in file_name and '.csv' in link['href']:
-        url = link['href']
-        csvYr = file_name.replace('Spending ', '').strip()[-4:]
-        csvMth = file_name.replace('Spending ', '').strip()[:3]
-        csvMth = convert_mth_strings(csvMth.upper())
-        data.append([csvYr, csvMth, url])
+    if 'http' not in link['href']:
+        year_url = 'https://www.breckland.gov.uk' + link['href']
+    else:
+        year_url = link['href']
+    year_html = urllib2.urlopen(year_url)
+    year_soup = BeautifulSoup(year_html, 'lxml')
+    blocks = year_soup.find_all('a', 'embeddoc__link')
+    for block in blocks:
+        if '.csv' in block['href'] or '.xlx' in block['href'] or '.xlsx' in block['href']:
+            url = block['href']
+            if 'GPC' not in url:
+                if 'http' not in url:
+                    url = 'https://www.breckland.gov.uk' + url
+                else:
+                    url = url
+                file_name = block.text.split('.')[-2].split('_')[0]
+                csvMth = file_name[:3]
+                csvYr = file_name.split()[1]
+                csvMth = convert_mth_strings(csvMth.upper())
+                data.append([csvYr, csvMth, url])
 
 
 #### STORE DATA 1.0
